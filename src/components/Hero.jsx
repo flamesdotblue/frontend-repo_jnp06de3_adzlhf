2abb2039e2a701ac9b3d0f373a8cb623ecbb22e0
Flames <vibe-coder@flames.com>
2025-10-26 16:52:48 +0000

    Update project prj_jnp06de3 - 2025-10-26T16:52:47.911Z

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({ onGetStarted }) => {
       {/* Spline Scene */}
       <div className="absolute inset-0">
         <Spline
-          scene="https://prod.spline.design/VJLoxp84lCdVfdZu/scene.splinecode"
+          scene="https://prod.spline.design/Gt5HUob8aGDxOUep/scene.splinecode"
           style={{ width: '100%', height: '100%' }}
         />
       </div>
